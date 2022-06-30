c167cae2bf8888efec69009b2d0d059c59abf553
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2022-06-30 13:49:57 +0530

    Add file upload/download support

--- Deloitte_Project/Migrations/20220630071830_migration2.Designer.cs
+++ Deloitte_Project/Migrations/20220630071830_migration2.Designer.cs
@@ -3,14 +3,16 @@ using Deloitte_Project.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Deloitte_Project.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220630071830_migration2")]
+    partial class migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
