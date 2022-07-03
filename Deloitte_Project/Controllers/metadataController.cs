c06a3b272c75c231868db75f87380c3798cb391f
SnehaDharne <55712542+SnehaDharne@users.noreply.github.com>
2022-07-03 14:02:05 +0530

    show metadata from UI, dynamic list of all files

--- Deloitte_Project/Controllers/metadataController.cs
+++ Deloitte_Project/Controllers/metadataController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Deloitte_Project.Models;
-
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using Microsoft.AspNetCore.Cors;
+using System.Net.Mail;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Deloitte_Project.Controllers
@@ -21,27 +26,62 @@ namespace Deloitte_Project.Controllers
         }
 
         // GET: api/<MetadataController>
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Metadata>>> GetMetadata()
-        {
-            return await _context.Metadatas.ToListAsync();
-        }
-
-        // GET api/<MetadataController>/5
+        [HttpGet(nameof(GetMetadata))]
         [EnableCors("Policy1")]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Metadata>> GetDetails(string Id)
+        public async Task<ActionResult<String[]>> GetMetadata()
         {
-            var user = await _context.Users.FindAsync(Id);
-            if (user == null)
+            var sess = await _context.Metadatas.Where(c => c.Id > 0).ToListAsync();
+            string[] result = new String[sess.Count];   
+            for(int i=0; i<sess.Count; i++)
+            {
+                result[i] = sess[i].file_name.ToString();
+            }
+            return result;
+        }
+        //public async Task<ActionResult<String>> GetlastName()
+        //{
+        //    //var sess = await _context.Sessions.FindAsync(Id);
+        //    var sess = await _context.Sessions.Where(c => c.isDeleted == false).ToListAsync();
+
+        //    return sess[0].lastName;
+        //}
+    // GET api/<MetadataController>/5
+        [EnableCors("Policy1")]
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<String[]>> GetCreatedBy(int Id)
+        {
+            var met = await _context.Metadatas.FindAsync(Id);
+            if (met == null)
             {
                 return NotFound();
             }
+            
             else
             {
-                return BadRequest();
+
+                string[] result = new String[3];
+
+                result[0] = met.created_by.ToString();
+                result[1] = met.created_on.ToString();
+                result[2] = met.file_name.ToString();
+                return result;
+
+                
             }
         }
+
+        //public async Task<ActionResult<Metadata>> GetCreatedOn(string file_name)
+        //{
+        //    var met = await _context.Metadatas.FindAsync(file_name);
+        //    if (met == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        return Ok(met.created_on);
+        //    }
+        //}
 
         // POST api/<MetadataController>
         [HttpPost]
