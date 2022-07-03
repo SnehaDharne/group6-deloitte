60da2e6168c401519a3c56af73414ab115e3a946
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2022-07-03 09:52:35 +0530

    fixup contact and remove unneeded paramters ( migration create and update needed )

--- Deloitte_Project/Migrations/20220703041659_m1.Designer.cs
+++ Deloitte_Project/Migrations/20220703041659_m1.Designer.cs
@@ -3,14 +3,16 @@ using Deloitte_Project.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Deloitte_Project.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220703041659_m1")]
+    partial class m1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Deloitte_Project/Models/metadata.cs
+++ Deloitte_Project/Models/metadata.cs
@@ -4,17 +4,12 @@
     {
         public int Id { get; set; }
         public string file_name { get; set; }
-        public long file_size { get; set; }
-        public string sub_directory { get; set; }
         public string created_on { get; set; }
         public string created_by { get; set; }
     }
 }
 //{
-//    "id": 123,
-//    "file_name": "svb",
-//    "file_size": 12,
-//    "sub_directory": "qergtdng",
+//    "file_name": "sample",
 //    "created_on": "egsv",
 //    "created_by": "qwaegsf"
 //}
